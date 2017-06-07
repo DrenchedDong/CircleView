9e737478758ea8f4d60620baec31ebf0af3d5220
DrenchedDong <2293451355@qq.com>
2017-06-07 13:55:49 +0800

    添加仅点击圆形才可移动的两种方法

--- app/src/main/java/dongting/bwei/com/circleview/MyCircle.java
+++ app/src/main/java/dongting/bwei/com/circleview/MyCircle.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,6 +20,7 @@ public class MyCircle extends View {
     private final Paint paint;
     private float x =100;
     private float y =100;
+    private final Rect rect;
 
     public MyCircle(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -28,13 +30,16 @@ public class MyCircle extends View {
         paint.setColor(Color.RED);
         paint.setAntiAlias(true);
 
+        rect = new Rect();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(x,y,10,paint);
+        canvas.drawCircle(x,y,50,paint);
+
+        rect.set((int)x-100,(int)y-100,(int)x+100,(int)y+100);
     }
 
     @Override
@@ -47,15 +52,45 @@ public class MyCircle extends View {
 
             case MotionEvent.ACTION_MOVE:
 
-                x = event.getX();
+                //点击屏幕任何地方都可拖动圆形
+
+               /* x = event.getX();
                 y = event.getY();
-                /*System.out.println("x = " + x);
+                *//*System.out.println("x = " + x);
                 System.out.println("y = " + y);
-*/
+*//*
                 //主线程调用刷新
                 invalidate();
                 //子线程 调用刷新
-//                postInvalidate();
+//                postInvalidate();*/
+
+
+                //点击圆形区域才可拖动圆形 1.勾股定理两点距离
+
+               /* float x_t=event.getX();
+                float y_t=event.getY();
+                float t=(x_t-x)*(x_t-x)+(y_t-y)*(y_t-y);
+
+                float sqrt = (float) Math.sqrt(t);
+                if(sqrt<=50){
+                    x=event.getX();
+                    y=event.getY();
+                    invalidate();
+                }*/
+
+
+                //点击圆形区域才可拖动圆形 2.利用矩形
+
+                float x_t=event.getX();
+                float y_t=event.getY();
+
+                if(x_t>rect.left && x_t<rect.right && y_t>rect.top && y_t<rect.bottom){
+
+                    x=event.getX();
+                    y=event.getY();
+                    invalidate();
+                }
+
 
                 break;
             case MotionEvent.ACTION_UP:
